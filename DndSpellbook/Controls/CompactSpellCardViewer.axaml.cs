e0f4e76f46440516e04df0083f2ac703606726a1
Earlh21 <idrialite@gmail.com>
2025-02-19 16:02:18 -0500

    Add optional cards view, add compact card view

---
+++ DndSpellbook/Controls/CompactSpellCardViewer.axaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using DndSpellbook.Data.Models;
+
+namespace DndSpellbook.Controls;
+
+public partial class CompactSpellCardViewer : UserControl
+{
+    public static readonly StyledProperty<Spell> SpellProperty =
+        AvaloniaProperty.Register<SpellCardViewer, Spell>(nameof(Spell));
+
+    public Spell Spell
+    {
+        get => GetValue(SpellProperty);
+        set => SetValue(SpellProperty, value);
+    }
+    
+    public static readonly StyledProperty<ICommand?> DeleteCommandProperty =
+        AvaloniaProperty.Register<SpellCardViewer, ICommand?>(nameof(DeleteCommand));
+
+    public ICommand? DeleteCommand
+    {
+        get => GetValue(DeleteCommandProperty);
+        set => SetValue(DeleteCommandProperty, value);
+    }
+    
+    public CompactSpellCardViewer()
+    {
+        InitializeComponent();
+    }
+}
