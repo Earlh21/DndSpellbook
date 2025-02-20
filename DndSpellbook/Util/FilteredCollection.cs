a08c1ea2d38bf54cee80fc04c0e35afb9674da2b
Earlh21 <idrialite@gmail.com>
2025-02-20 10:36:10 -0500

    Add filtering, spell importing

---
+++ DndSpellbook/Util/FilteredCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DynamicData;
+using DynamicData.Binding;
+
+namespace DndSpellbook.Util;
+
+public class FilteredCollection<T> where T : notnull
+{
+    private readonly SourceCache<T, int> sourceCache;
+    private ReadOnlyObservableCollection<T> view;
+    
+    public ReadOnlyObservableCollection<T> View => view;
+    public IReadOnlyList<T> AllItems => sourceCache.Items;
+    
+    public FilteredCollection(Func<T, int> idSelector, SortExpressionComparer<T>? sorter, params IObservable<Func<T, bool>>[] filters)
+    {
+        sourceCache = new(idSelector);
+
+        var observable = sourceCache.Connect();
+        
+        foreach (var filter in filters)
+        {
+            observable = observable.Filter(filter);
+        }
+
+        if (sorter == null)
+        {
+            observable.Bind(out view).Subscribe();
+        }
+        else
+        {
+            observable.SortAndBind(out view, sorter).Subscribe();
+        }
+    }
+    
+    public FilteredCollection(Func<T, int> idSelector, params IObservable<Func<T, bool>>[] filters)
+    {
+        sourceCache = new(idSelector);
+
+        var observable = sourceCache.Connect();
+        
+        foreach (var filter in filters)
+        {
+            observable = observable.Filter(filter);
+        }
+
+        observable.Bind(out view).Subscribe();
+    }
+
+    public void Remove(T item)
+    {
+        sourceCache.Remove(item);
+    }
+    
+    public void RemoveKey(int key)
+    {
+        sourceCache.RemoveKey(key);
+    }
+    
+    public void AddOrUpdate(T item)
+    {
+        sourceCache.AddOrUpdate(item);
+    }
+    
+    public void AddOrUpdate(IEnumerable<T> items)
+    {
+        sourceCache.AddOrUpdate(items);
+    }
+    
+    public void ReplaceAll(IEnumerable<T> items)
+    {
+        sourceCache.Edit(innerCache =>
+        {
+            innerCache.Clear();
+            innerCache.AddOrUpdate(items);
+        });
+    }
+    
+    public void Clear()
+    {
+        sourceCache.Clear();
+    }
+}
--- DndSpellbook/Views/Spells/SpellsViewModel.cs
+++ DndSpellbook/Views/Spells/SpellsViewModel.cs
@@ -8,9 +8,12 @@ using System.Reactive.Linq;
 using System.Threading.Tasks;
 using DndSpellbook.Controls;
 using DndSpellbook.Data.Models;
+using DndSpellbook.Data.Models.Enums;
 using DndSpellbook.Data.Services;
 using DndSpellbook.Import;
 using DndSpellbook.Navigation;
+using DndSpellbook.Util;
+using DynamicData.Binding;
 using ReactiveUI;
 using Splat;
 
@@ -20,6 +23,46 @@ public class SpellsViewModel : ViewModelBase, IDialog
 {
     private readonly SpellService spellService;
     private readonly SpellListService spellListService;
+
+    private int? filterMinLevel;
+
+    public int? FilterMinLevel
+    {
+        get => filterMinLevel;
+        set => this.RaiseAndSetIfChanged(ref filterMinLevel, value);
+    }
+
+    private int? filterMaxLevel;
+
+    public int? FilterMaxLevel
+    {
+        get => filterMaxLevel;
+        set => this.RaiseAndSetIfChanged(ref filterMaxLevel, value);
+    }
+
+    private SpellSchool? filterSchool;
+
+    public SpellSchool? FilterSchool
+    {
+        get => filterSchool;
+        set => this.RaiseAndSetIfChanged(ref filterSchool, value);
+    }
+
+    private SpellList? filterSpellList;
+
+    public SpellList? FilterSpellList
+    {
+        get => filterSpellList;
+        set => this.RaiseAndSetIfChanged(ref filterSpellList, value);
+    }
+
+    private string filterText = "";
+
+    public string FilterText
+    {
+        get => filterText;
+        set => this.RaiseAndSetIfChanged(ref filterText, value);
+    }
 
     private bool isSelector;
 
@@ -37,9 +80,8 @@ public class SpellsViewModel : ViewModelBase, IDialog
         set => this.RaiseAndSetIfChanged(ref isCardView, value);
     }
 
-    private ObservableCollection<SpellCardViewModel> spells = new();
-
-    public ObservableCollection<SpellCardViewModel> Spells
+    private FilteredCollection<SpellCardViewModel> spells;
+    public FilteredCollection<SpellCardViewModel> Spells
     {
         get => spells;
         set => this.RaiseAndSetIfChanged(ref spells, value);
@@ -52,6 +94,15 @@ public class SpellsViewModel : ViewModelBase, IDialog
         get => spellLists;
         set => this.RaiseAndSetIfChanged(ref spellLists, value);
     }
+    
+    private ObservableCollection<SpellList?> spellListsWithNull = new();
+    public ObservableCollection<SpellList?> SpellListsWithNull
+    {
+        get => spellListsWithNull;
+        set => this.RaiseAndSetIfChanged(ref spellListsWithNull, value);
+    }
+    
+    public SpellSchool?[] SpellSchools => Enum.GetValues<SpellSchool>().Select(x => (SpellSchool?)x).Prepend(null).ToArray();
 
     public ReactiveCommand<Unit, Unit> NewSpellCommand { get; }
     public ReactiveCommand<SpellCardViewModel, Unit> DeleteSpellCommand { get; }
@@ -70,12 +121,60 @@ public class SpellsViewModel : ViewModelBase, IDialog
         IsSelector = asSelector;
         IsCardView = asCardView;
 
+        Spells = CreateFiltered(null);
+
         NewSpellCommand = ReactiveCommand.CreateFromTask(NewSpell);
         DeleteSpellCommand = ReactiveCommand.CreateFromTask<SpellCardViewModel>(DeleteSpell);
         SaveCommand = ReactiveCommand.Create(Save);
         CancelCommand = ReactiveCommand.Create(Cancel);
         ImportSpellsCommand = ReactiveCommand.CreateFromTask(ImportSpells);
         ClearSpellsCommand = ReactiveCommand.CreateFromTask(ClearSpells);
+    }
+
+    private FilteredCollection<SpellCardViewModel> CreateFiltered(SortExpressionComparer<SpellCardViewModel>? sorter)
+    {
+        Func<SpellCardViewModel, bool> levelFilter = spell =>
+        {
+            if (FilterMinLevel != null && spell.Spell.Level < FilterMinLevel) return false;
+            if (FilterMaxLevel != null && spell.Spell.Level > FilterMaxLevel) return false;
+            return true;
+        };
+
+        Func<SpellCardViewModel, bool> schoolFilter = spell =>
+        {
+            if (FilterSchool != null && spell.Spell.School != FilterSchool) return false;
+            return true;
+        };
+
+        Func<SpellCardViewModel, bool> listFilter = spell =>
+        {
+            if (FilterSpellList != null && !spell.Spell.SpellLists.Contains(FilterSpellList)) return false;
+            return true;
+        };
+
+        Func<SpellCardViewModel, bool> textFilter = spell =>
+        {
+            if (String.IsNullOrWhiteSpace(FilterText)) return true;
+
+            var searchWords = FilterText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in searchWords)
+            {
+                if (!spell.Spell.Name.Contains(word, StringComparison.OrdinalIgnoreCase) &&
+                    !spell.Spell.Description.Contains(word, StringComparison.OrdinalIgnoreCase)) return false;
+            }
+
+            return true;
+        };
+
+        return new FilteredCollection<SpellCardViewModel>(x => x.Spell.Id,
+                sorter,
+                this.WhenValueChanged(vm => vm.FilterMinLevel).Select(_ => levelFilter),
+                this.WhenValueChanged(vm => vm.FilterMaxLevel).Select(_ => levelFilter),
+                this.WhenValueChanged(vm => vm.FilterSchool).Select(_ => schoolFilter),
+                this.WhenValueChanged(vm => vm.FilterSpellList).Select(_ => listFilter),
+                this.WhenValueChanged(vm => vm.FilterText).Select(_ => textFilter)
+            );
     }
 
     public async Task LoadDataAsync()
@@ -85,10 +184,11 @@ public class SpellsViewModel : ViewModelBase, IDialog
 
         var spellListArray = fetchedSpellLists.ToArray();
 
-        Spells = new(fetchedSpells.Select(s =>
+        Spells.ReplaceAll(fetchedSpells.Select(s =>
             new SpellCardViewModel(s, spellListArray, spellService, IsSelector, DeleteSpellCommand)));
 
         SpellLists = new(fetchedSpellLists);
+        SpellListsWithNull = new(fetchedSpellLists.Prepend(null));
     }
 
     private async Task DeleteSpell(SpellCardViewModel spell)
@@ -102,7 +202,7 @@ public class SpellsViewModel : ViewModelBase, IDialog
         var spell = new Spell("Name");
         var spellCard = new SpellCardViewModel(spell, spellLists.ToArray(), spellService, IsSelector,
             DeleteSpellCommand);
-        Spells.Insert(0, spellCard);
+        Spells.AddOrUpdate(spellCard);
 
         await spellService.AddAsync(spell);
     }
@@ -117,7 +217,7 @@ public class SpellsViewModel : ViewModelBase, IDialog
 
         await spellService.AddAsync(data.NewSpells);
     }
-    
+
     private async Task ClearSpells()
     {
         await spellService.ClearAsync();
@@ -126,7 +226,7 @@ public class SpellsViewModel : ViewModelBase, IDialog
 
     private void Save()
     {
-        Closed?.Invoke(this, Spells.Where(s => s.IsSelected).Select(s => s.Spell.Id));
+        Closed?.Invoke(this, Spells.AllItems.Where(s => s.IsSelected).Select(s => s.Spell.Id));
     }
 
     private void Cancel()
