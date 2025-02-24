286a3ff187e9fe163a3e71c0195232b2bf303f36
Earlh21 <idrialite@gmail.com>
2025-02-24 14:39:20 -0500

    Stuff

---
+++ DndSpellbook/Controls/Spells/Expanders/SpellExpanderEditor.axaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DndSpellbook.Controls;
+
+public partial class SpellExpanderEditor : UserControl
+{    
+    public static readonly StyledProperty<SpellEditor> SpellEditorProperty =
+        AvaloniaProperty.Register<SpellCardEditor, SpellEditor>(nameof(SpellEditor));
+
+    public SpellEditor SpellEditor
+    {
+        get => GetValue(SpellEditorProperty);
+        set => SetValue(SpellEditorProperty, value);
+    }
+    public SpellExpanderEditor()
+    {
+        InitializeComponent();
+    }
+}
